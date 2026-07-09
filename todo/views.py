b9159c1104b3b12cab9d17b044a908af09953916
1f10250051 <s1f102500517@iniad.org>
2026-07-09 11:37:08 +0900

    タスク終了機能を追加

--- todo/views.py
+++ todo/views.py
@@ -48,3 +48,12 @@ def update(request, task_id):
         'task': task
     }
     return render(request, 'todo/edit.html', context)
+
+def close(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404('Task dose not exist')
+    task.completed = True
+    task.save()
+    return redirect(index)
