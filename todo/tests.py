783dd8f2ed7045b28dd1acb2e95911ff48fdbfcb
kawahara <s1f102500988@iniad.org>
2026-07-09 11:21:53 +0900

    edit_page-test

--- todo/tests.py
+++ todo/tests.py
@@ -107,3 +107,26 @@ class TaskViewTestCase(TestCase):
         client = Client()
         response = client.get('/1/')
         self.assertEqual(response.status_code, 404)
+
+    def test_update_get_success(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task.save()
+        client = Client()
+        response = client.get('/{}/edit/'.format(task.pk))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/edit.html')
+        self.assertEqual(response.context['task'], task)
+
+    def test_update_post_success(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task.save()
+        client = Client()
+        data = {'title': 'updated task', 'due_at': '2024-08-01 23:59:59'}
+        response = client.post('/{}/edit/'.format(task.pk), data)
+
+        task.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/{}/'.format(task.pk))
+        self.assertEqual(task.title, 'updated task')
+        self.assertEqual(task.due_at, timezone.make_aware(datetime(2024, 8, 1, 23, 59, 59)))
