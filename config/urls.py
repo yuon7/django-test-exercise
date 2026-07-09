a10b2ec2f7f25262f71cf10f29b4bdc10bfe785f
yuon7 <131662887+yuon7@users.noreply.github.com>
2026-07-09 11:37:56 +0900

    Revert "Delete"

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', todo_views.index, name='index'),
     path('<int:task_id>/', todo_views.detail, name='detail'),
-    path('<int:task_id>/delete', todo_views.delete, name='delete'),
-    path('<int:task_id>/edit/', todo_views.update, name='update'),
+    path('<int:task_id>/edit/', todo_views.update, name='update')
 ]
